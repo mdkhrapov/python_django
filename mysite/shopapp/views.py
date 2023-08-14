4ff9896b6f5e7b468a14b8d5ec8fd6ef564840df
Михаил Храпов <mikhail.khrapov@msgplaut.com>
2023-08-14 22:09:55 +0300

    Django project "mysite", 14.08.2023

--- mysite/shopapp/views.py
+++ mysite/shopapp/views.py
@@ -1,7 +1,9 @@
 from timeit import default_timer
 from datetime import date
 from django.shortcuts import render
+from django.contrib.auth.models import Group
 from django.http import HttpResponse, HttpRequest
+from .models import Product, Order
 
 def shop_index(request: HttpRequest):
     products = [
@@ -11,11 +13,26 @@ def shop_index(request: HttpRequest):
         ('monitor', 1200),
         ('printer', 1500),
     ]
-
     context = {
-        "cur_date": date.today(),
         "time_running": default_timer(),
         "products": products,
         }
-
     return render(request, 'shopapp/shop-index.html', context=context)
+
+def groups_list(request: HttpRequest):
+    context = {
+        "groups": Group.objects.prefetch_related('permissions').all(),
+        }
+    return render(request, 'shopapp/groups-list.html', context=context)
+
+def products_list(request: HttpRequest):
+    context = {
+         "products": Product.objects.all(),
+        }
+    return render(request, 'shopapp/products-list.html', context=context)
+
+def orders_list(request: HttpRequest):
+    context = {
+         "orders": Order.objects.select_related("user").prefetch_related("products").all(),
+        }
+    return render(request, 'shopapp/orders-list.html', context=context)
