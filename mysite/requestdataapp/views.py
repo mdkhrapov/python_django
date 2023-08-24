3b3956d49abe9c778621ae4aaa2520c541873bc0
Михаил Храпов <mikhail.khrapov@msgplaut.com>
2023-08-24 17:42:21 +0300

    Django project "mysite", 24.08.2023

--- mysite/requestdataapp/views.py
+++ mysite/requestdataapp/views.py
@@ -1,6 +1,7 @@
 from django.core.files.storage import FileSystemStorage
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+from .forms import UserBioForm, UploadFileForm
 
 # Create your views here.
 
@@ -15,22 +16,32 @@ def process_get_view(request: HttpRequest) -> HttpResponse:
     return render(request, "requestdataapp/request-query-params.html", context=context)
 
 def user_form(request: HttpRequest) -> HttpResponse:
-    context = {}
+    context = { "form": UserBioForm(), }
     return render(request, "requestdataapp/user-bio-form.html", context=context)
 
+def upload_file_form(request: HttpRequest) -> HttpResponse:
+    context = { "form": UploadFileForm(), }
+    return render(request, "requestdataapp/file_upload", context=context)
+
 def handle_file_upload(request: HttpRequest) -> HttpResponse:
-    context = {}
-    if request.method == "POST" and request.FILES.get("myfile"):
-        myfile = request.FILES["myfile"]
-        fs = FileSystemStorage()
-        filename = fs.save(myfile.name, myfile)
-        filesize_mb = int(fs.size(myfile.name))/(1024 * 1024)     # переводим в Мб
-        context = {"filesize": filesize_mb}
+    filesize_mb = 0
 
-        if filesize_mb > 1:
-            fs.delete(myfile.name)
-            print("file was delete")
-            return render(request, "requestdataapp/error-upload.html", context=context)
+    if request.method == "POST":
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            #myfile = request.FILES["myfile"]
+            myfile = form.cleaned_data["file"]
+            fs = FileSystemStorage()
+            filename = fs.save(myfile.name, myfile)
+            filesize_mb = int(fs.size(myfile.name))/(1024 * 1024)     # переводим в Мб
+            print(f"saved file: {filename}")
 
-        print(f"saved file: {filename}")
+            if filesize_mb > 1:
+                fs.delete(myfile.name)
+                print("file was delete")
+                return render(request, "requestdataapp/error-upload.html", context=context)
+    else:
+        form = UploadFileForm()
+
+    context = {"form": form, "filesize":filesize_mb}
     return render(request, "requestdataapp/file-upload.html", context=context)
