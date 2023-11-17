3138a250a5fdf8953026d597dbaaef25be0b5f94
Михаил Храпов <mikhail.khrapov@msgplaut.com>
2023-11-17 13:13:13 +0300

    Django project "mysite", 17.11.2023

--- queries/mysite/blogapp/admin.py
+++ queries/mysite/blogapp/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from django.db.models import QuerySet
+from django.http import HttpRequest
 
-# Register your models here.
+from .models import Article, Tag
+
+class ArticleInline(admin.TabularInline):
+    model = Tag.tag.through
+
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    inlines = [
+        ArticleInline,
+    ]
+    list_display = "title", "content", "pub_date", "author", "category"
+
+    def get_queryset(self, request):
+        return Article.objects.select_related("category").prefetch_related("tags").all()
+
+
